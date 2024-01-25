12e63b64caee3f951a14bd144ef0c1594b06c75e
Vadym <petrukvadd@gmail.com>
2024-01-25 17:19:28 +0200

    first commit

---
+++ women/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Women(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    time_create = models.DateField(auto_now_add=True)
+    time_update = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey("Category", on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+
+    def __str__(self):
+        return self.name
