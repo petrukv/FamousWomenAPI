c44c662c344735a26232557ec5f2ebb6221a233a
Vadym <petrukvadd@gmail.com>
2024-01-28 19:13:03 +0200

    implemented JWT

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework import routers
+from rest_framework_simplejwt.views import *
 
 from women import views
 
@@ -31,6 +32,9 @@ urlpatterns = [
     path('api/v1/womendelete/<int:pk>/', views.WomenAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('api/v1/womenlist/', views.WomenViewSet.as_view({'get':'list'})),
     # path('api/v1/womenlist/<int:pk>/', views.WomenViewSet.as_view({'put':'update'})),
     # path('api/v1/womendetail/<int:pk>/', views.WomenAPIDetailView.as_view()),
--- women/views.py
+++ women/views.py
@@ -24,7 +24,7 @@ class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication,)
+    # authentication_classes = (TokenAuthentication,)
 
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
